7f8a3edc2b2fb5b6dec39b54eb8cc7ab7b338ddd
JeanAtCismet <jean.ruiz@cismet.de>
2015-01-14 16:15:38 +0100

    fixed #17 added ewkt support by converting the incomming wkt if srid is set

--- src/main/java/de/cismet/belis2/server/search/BelisObjectsWktSearch.java
+++ src/main/java/de/cismet/belis2/server/search/BelisObjectsWktSearch.java
@@ -12,9 +12,14 @@
  */
 package de.cismet.belis2.server.search;
 
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.PrecisionModel;
 import com.vividsolutions.jts.io.WKTReader;
 
 import de.cismet.cids.server.search.CidsServerSearch;
+
+import de.cismet.commons.converter.ConversionException;
 
 /**
  * DOCUMENT ME!
@@ -25,10 +30,6 @@ import de.cismet.cids.server.search.CidsServerSearch;
 @org.openide.util.lookup.ServiceProvider(service = CidsServerSearch.class)
 public class BelisObjectsWktSearch extends BelisSearchStatement {
 
-    //~ Instance fields --------------------------------------------------------
-
-    private final transient WKTReader wktReater = new WKTReader();
-
     //~ Methods ----------------------------------------------------------------
 
     /**
@@ -38,7 +39,40 @@ public class BelisObjectsWktSearch extends BelisSearchStatement {
      *
      * @throws  Exception  DOCUMENT ME!
      */
-    public void setGeometryFromWkt(final String wktGeometry) throws Exception {
-        setGeometry(wktReater.read(wktGeometry));
+    public void setGeometryFromWkt(final String wktGeometry) throws Exception {              
+        final int skIndex = wktGeometry.indexOf(';');
+        final String wkt;
+        final int srid;
+        if (skIndex > 0) {
+            final String sridKV = wktGeometry.substring(0, skIndex);
+            final int eqIndex = sridKV.indexOf('=');
+
+            if (eqIndex > 0) {
+                srid = Integer.parseInt(sridKV.substring(eqIndex + 1));
+                wkt = wktGeometry.substring(skIndex + 1);
+            } else {
+                wkt = wktGeometry;
+                srid = -1;
+            }
+        } else {
+            wkt = wktGeometry;
+            srid = -1;
+        }
+
+        final Geometry geometry;
+
+        if (srid < 0) {
+            geometry = new WKTReader().read(wkt);
+        } else {
+            final GeometryFactory geomFactory = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), srid);
+            geometry = new WKTReader(geomFactory).read(wkt);
+        }
+
+        
+        if (geometry.getSRID() < 0) {
+            setGeometry(geometry);
+        } else {
+            setGeometry(CrsTransformer.transformToDefaultCrs(geometry));
+        }
     }
 }
