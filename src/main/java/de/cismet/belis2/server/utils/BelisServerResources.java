c4732d146accec65fb19e839cb0c5b991d5df84d
JeanAtCismet <jean.ruiz@cismet.de>
2016-10-26 16:48:40 +0200

    fixed #43 using new ServerResource classes

--- src/main/java/de/cismet/belis2/server/utils/BelisServerResources.java
+++ src/main/java/de/cismet/belis2/server/utils/BelisServerResources.java
@@ -12,7 +12,10 @@
  */
 package de.cismet.belis2.server.utils;
 
-import de.cismet.cids.utils.serverresources.ServerResourcesLoader;
+import lombok.Getter;
+
+import de.cismet.cids.utils.serverresources.ServerResource;
+import de.cismet.cids.utils.serverresources.TextServerResource;
 
 /**
  * DOCUMENT ME!
@@ -24,14 +27,13 @@ public enum BelisServerResources {
 
     //~ Enum constants ---------------------------------------------------------
 
-    MOTD_BELIS2_PROPERTIES("/motd/belis2.properties", ServerResourcesLoader.Type.TEXT),
+    MOTD_BELIS2_PROPERTIES(new TextServerResource("/motd/belis2.properties")),
 
-    WEBDAV("/webdav/WebDav.properties", ServerResourcesLoader.Type.TEXT);
+    WEBDAV(new TextServerResource("/webdav/WebDav.properties"));
 
     //~ Instance fields --------------------------------------------------------
 
-    private final String value;
-    private final ServerResourcesLoader.Type type;
+    @Getter private final ServerResource value;
 
     //~ Constructors -----------------------------------------------------------
 
@@ -39,55 +41,8 @@ public enum BelisServerResources {
      * Creates a new Props object.
      *
      * @param  value  DOCUMENT ME!
-     * @param  type   DOCUMENT ME!
      */
-    BelisServerResources(final String value, final ServerResourcesLoader.Type type) {
+    BelisServerResources(final ServerResource value) {
         this.value = value;
-        this.type = type;
-    }
-
-    //~ Methods ----------------------------------------------------------------
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public String getValue() {
-        return value;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public ServerResourcesLoader.Type getType() {
-        return type;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     *
-     * @throws  Exception  DOCUMENT ME!
-     */
-    public Object loadServerResources() throws Exception {
-        final ServerResourcesLoader loader = ServerResourcesLoader.getInstance();
-        switch (type) {
-            case JASPER_REPORT: {
-                return loader.loadJasperReportResource(value);
-            }
-            case TEXT: {
-                return loader.loadTextResource(value);
-            }
-            case BINARY: {
-                return loader.loadBinaryResource(value);
-            }
-            default: {
-                throw new Exception("unknown serverResource type");
-            }
-        }
     }
 }
